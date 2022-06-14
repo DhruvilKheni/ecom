4205aac0b13ca5faa54eb7c9b7b665c8e6a5bb95
Dhruvil <dhruvilkheni89@gmail.com>
2022-06-14 20:19:07 +0530

    header

--- buyer/urls.py
+++ buyer/urls.py
@@ -3,6 +3,11 @@ from buyer import views
 
 urlpatterns = [
     path('',views.index,name = 'index'),
+    path('shop',views.shop,name = 'shop'),
+    path('cart',views.cart,name = 'cart'),
+    path('detail',views.detail,name = 'detail'),
+    path('checkout',views.checkout,name = 'checkout'),
+    path('contact',views.contact,name = 'contact'),
 ]
 
 
--- buyer/views.py
+++ buyer/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 def index(request):
     
     return render(request,'index.html')
+
+def checkout(request):
+    
+    return render(request,'checkout.html')
+
+def detail(request):
+    
+    return render(request,'detail.html')
+
+def shop(request):
+    
+    return render(request,'shop.html')    
+
+def cart(request):
+    
+    return render(request,'cart.html')
+
+def contact(request):
+    
+    return render(request,'contact.html')
